a7d990926edc3830745a2c93f0aeac6ea7608ebb
ridho <ridho.putra@btpn.com>
2023-01-09 15:42:58 +0700

    create server from socket io & add user to join room

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const cors = require('cors');
+const { Server } = require('socket.io'); // Add this
 
 app.use(cors()); // Add cors middleware
 
 const server = http.createServer(app);
 
-// Add this
 // Create an io server and allow for CORS from http://localhost:3000 with GET and POST methods
 const io = new Server(server, {
     cors: {
@@ -16,9 +16,15 @@ const io = new Server(server, {
     },
 });
 
-// Add this
-app.get('/', (req, res) => {
-    res.send('Hello world');
+// Listen for when the client connects via socket.io-client
+io.on('connection', (socket) => {
+    console.log(`User connected ${socket.id}`);
+
+    // Add a user to a room
+    socket.on('join_room', (data) => {
+        const { username, room } = data; // Data sent from client when join_room event emitted
+        socket.join(room); // Join the user to a socket room
+    });
 });
 
 server.listen(4000, () => 'Server is running on port 4000');
